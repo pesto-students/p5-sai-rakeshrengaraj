8608a2644a485f28d9d4b9b7aa27117fc7abae4d
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2022-09-28 20:15:29 +0530

    function to find pair with given difference

--- Week-6/Assignment-5/assignment_5.js
+++ Week-6/Assignment-5/assignment_5.js
@@ -1,14 +1,16 @@
 
+// Pair With Given Differenc
+
+// Time Complexity:- O(n*2)
+// Space Complexity:- O(1)
 
 
 function pairDiff(numArr, target){
     let arr = numArr.sort((a, b) => a - b);
-    for(let i = 0; i <= arr.length ; i++){
-        for(let j=i+1; j<=arr.length; j++){
-            sum = arr[i] - arr[j]
-            if (Math.abs(sum) === target){
+    for(let i = 0; i < arr.length ; i++){
+        for(let j=i+1; j< arr.length; j++){
+            if ((arr[i] - arr[j] === target) || (arr[j] - arr[i]  === target))
                 return 1
-            }
         }
     }
     return 0
@@ -17,3 +19,5 @@ function pairDiff(numArr, target){
 
 console.log(pairDiff([5, 10, 3, 2, 50, 80], 78)) // 1
 console.log(pairDiff([-10, 20], 30)) // 1
+
+
