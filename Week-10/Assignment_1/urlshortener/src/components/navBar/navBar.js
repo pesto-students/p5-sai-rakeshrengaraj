43fe44324f07034caa5e4c18400d090cfd55fe40
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2022-11-26 23:27:22 +0530

    Styled responsive for mobile devices

--- Week-10/Assignment_1/urlshortener/src/components/navBar/navBar.js
+++ Week-10/Assignment_1/urlshortener/src/components/navBar/navBar.js
@@ -1,35 +1,40 @@
-// import React from "react";
-import styles from './navBar.module.css'
-
-
-function navBarResponsive(){
-    var x = document.getElementById("navBarTop");
-    if (x.className === "navBar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navBar";
-    }
-}
-
+import { useState } from "react";
+import styles from "./navBar.module.css";
 
 function NavBar() {
-  return (
-      <nav className={styles.navBar} id="navBarTop">
-        <div className={styles.navBar_header}>Shortly</div>
-        <div className={styles.navBar_navBtns}>
-          <a href="#">Features</a>
-          <a href="#">Pricing</a>
-          <a href="#">Resources</a>
-        </div>
-        <div className={styles.navBar_loginBtns}>
-          <a href="#">Login</a>
-          <a href="#">Sign Up</a>
-        </div>
-        <a href="#" className={styles.icon} onClick={navBarResponsive}>
-            <i className="fa fa-bars"></i>
-          </a>
-      </nav>
+  const [clicked, setClicked] = useState(false);
 
+  const onClickHandler = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <nav className={styles.navBar}>
+      <div className={styles.navBar_header}>
+        <h5>Shortly</h5>
+        <a href="#" className={styles.icon} onClick={onClickHandler}>
+          <i className={clicked ? "fa fa-close" : "fa fa-bars"}></i>
+        </a>
+      </div>
+
+      <div className={styles.navBar_navBtns}>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">Resources</a>
+      </div>
+      <div className={styles.navBar_loginBtns}>
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+      </div>
+
+      <div className={styles.mobile_navBarbtns} id={clicked ? "active" : ""}>
+        <a href="#">Features</a>
+        <a href="#">Pricing</a>
+        <a href="#">Resources</a>
+        <a href="#">Login</a>
+        <a href="#">Sign Up</a>
+      </div>
+    </nav>
   );
 }
 
--- Week-10/Assignment_1/urlshortener/src/components/urlOutput/urlOutput.js
+++ Week-10/Assignment_1/urlshortener/src/components/urlOutput/urlOutput.js
@@ -1,34 +1,32 @@
-import styles from "./urlOutput.module.css"
-import { useState,useEffect } from "react";
+import styles from "./urlOutput.module.css";
+import { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
-
-const UrlResult = ({shortUrl,error}) => {
-
+const UrlResult = ({ shortUrl, error }) => {
   const [copied, setCopied] = useState(false);
 
   //  Runs to set the css of copy clipboard back to default after 1 second whenever text is copied
   useEffect(() => {
     const timer = setTimeout(() => {
       setCopied(false);
-    }, 1000);
+    }, 2000);
 
     return () => {
       clearTimeout(timer);
     };
   }, [copied]);
 
-   
   return (
     <div className={styles.urlOutput}>
       <div className={styles.displayOutput}>{shortUrl}</div>
 
       <CopyToClipboard text={shortUrl} onCopy={() => setCopied(true)}>
-        <button className={copied ? styles.copyOutput : styles.notCopied}>Copy</button>
+        <button className={copied ? styles.copyOutput : styles.notCopied}>
+          Copy
+        </button>
       </CopyToClipboard>
     </div>
   );
 };
-
 
 export default UrlResult;
