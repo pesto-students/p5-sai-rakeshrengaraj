b598ef067a9c5e8e0609626fc11a37f2e612c0cb
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2022-12-15 19:11:00 +0530

    replaced href for anchor tag to remove warnings

--- Week-10/Assignment_1/urlshortener/src/components/navBar/navBar.js
+++ Week-10/Assignment_1/urlshortener/src/components/navBar/navBar.js
@@ -12,27 +12,27 @@ function NavBar() {
     <nav className={styles.navBar}>
       <div className={styles.navBar_header}>
         <h5>Shortly</h5>
-        <a href="#" className={styles.icon} onClick={onClickHandler}>
+        <a href="/#" className={styles.icon} onClick={onClickHandler}>
           <i className={clicked ? "fa fa-close" : "fa fa-bars"}></i>
         </a>
       </div>
 
       <div className={styles.navBar_navBtns}>
-        <a href="#">Features</a>
-        <a href="#">Pricing</a>
-        <a href="#">Resources</a>
+        <a href="/#">Features</a>
+        <a href="/#">Pricing</a>
+        <a href="/#">Resources</a>
       </div>
       <div className={styles.navBar_loginBtns}>
-        <a href="#">Login</a>
-        <a href="#">Sign Up</a>
+        <a href="/#">Login</a>
+        <a href="/#">Sign Up</a>
       </div>
 
       <div className={styles.mobile_navBarbtns} id={clicked ? "active" : ""}>
-        <a href="#">Features</a>
-        <a href="#">Pricing</a>
-        <a href="#">Resources</a>
-        <a href="#">Login</a>
-        <a href="#">Sign Up</a>
+        <a href="/#">Features</a>
+        <a href="/#">Pricing</a>
+        <a href="/#">Resources</a>
+        <a href="/#">Login</a>
+        <a href="/#">Sign Up</a>
       </div>
     </nav>
   );
