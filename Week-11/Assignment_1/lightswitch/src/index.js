07eb961f9bc3e9df299a64db8a799820e2db6313
rakeshrengaraj <rakeshrengaraj@bitbucket.org>
2022-12-20 14:19:51 +0530

    removed unwanted comments

--- Week-11/Assignment_1/lightswitch/src/index.js
+++ Week-11/Assignment_1/lightswitch/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Room from "./room";
 import RoomSwitch from "./roomSwitch"
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,37 +9,14 @@ import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
 
-// ******** For Class Redux Hooks ***************8
 import roomSwitchStore from "./redux/roomSwitchStore"
-// ******** For Class Redux Hooks ***************8
-
-
-// *********** For Redux Hooks **************************
-import { configureStore } from "@reduxjs/toolkit";
-import switchReducer from "./features/switch";
-
-
-const store = configureStore({
-  reducer: {
-    switch: switchReducer,
-  },
-});
-// *********** For Redux Hooks **************************
-
-
-
-
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* %%%%%%%%% For Redux Hooks uncomment Room, Provider store and comment RoomSwitch, Provider roomSwitchStore %%%%%%%%%%*/}
-    {/* <Provider store={store}> */}
-      {/* <Room />   */}
     <Provider store={roomSwitchStore}>
-      
       <RoomSwitch/>
     </Provider>
   </React.StrictMode>
